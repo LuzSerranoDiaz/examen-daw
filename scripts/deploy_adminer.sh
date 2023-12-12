053cdfd81efaecbfdd03a9c934246c14770cd51d
LuzSerranoDiaz <diaz03luz@gmail.com>
2023-12-12 11:40:28 +0000

    funciona

--- scripts/deploy_adminer.sh
+++ scripts/deploy_adminer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 apt install adminer -y
 
 wget https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php -P /var/www/html
+
+sudo systemctl enable apache2
+
+sudo a2enconf adminer
+
+sudo systemctl reload apache2
