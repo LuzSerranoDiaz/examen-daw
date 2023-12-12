086a31d3ff5a85479c7d8fef21409d38d7e2e826
LuzSerranoDiaz <diaz03luz@gmail.com>
2023-12-12 12:16:13 +0000

    a

--- scripts/deploy_adminer.sh
+++ scripts/deploy_adminer.sh
@@ -4,8 +4,6 @@ set -ex
 
 apt install adminer -y
 
-wget https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php -P /var/www/html
-
 sudo systemctl enable apache2
 
 sudo a2enconf adminer
